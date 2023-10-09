031eae230061ba2311690da0f8ad67eafd1f2537
clarencesarmiento <sarmiento.clarence1020@gmail.com>
2023-10-09 21:55:49 +0800

    Added Threading

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
 import customtkinter as ctk
 from tkinter import filedialog
 from tkinter import *
+from PIL import Image
 from pytube import YouTube
 import pytube.exceptions
 import os
 import threading
 
+# Set Application Appearance and Color Theme
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('blue')
 
+# Set Application Width and Height
 appWidth, appheight = 600, 300
+
+# Set Icons path
+current_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Assets')
+
+
+class App(ctk.CTk):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Place App in Center of Screen
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width - appWidth) // 2
+        y = (screen_height - appheight) // 2
+
+        # Create Window
+        self.geometry(f'{appWidth}x{appheight}+{x}+{y}')
+        self.resizable(False, False)
+        self.title('YouTube Downloader')
+        self.iconbitmap('Assets/Icon.ico')
+
+        # Configure App Column
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+        # Configure App Row
+        self.rowconfigure(1, weight=1)
+
+        # Add App Label
+        self.app_label = ctk.CTkLabel(self, text='YOUTUBE DOWNLOADER\n VIDEO | AUDIO',
+                                      font=('sans serif', 20, 'bold'))
+        self.app_label.grid(row=0, column=0, columnspan=2, padx=10, pady=(10, 0), sticky='ew')
+
+        # Create Frame
+        self.downloadframe = DownloadFrame(self)
+        self.downloadframe.grid(row=1, column=0, columnspan=2, padx=10, pady=(10, 0), sticky='nsew')
+
+        # Author
+        self.author = ctk.CTkLabel(self, text='C.Sarmiento 2023 Version 1.1', font=('helvetica', 10))
+        self.author.grid(row=2, column=0, padx=10, sticky='w')
+
+        # Run Application
+        self.mainloop()
 
 
 class DownloadFrame(ctk.CTkFrame):
@@ -22,6 +67,12 @@ class DownloadFrame(ctk.CTkFrame):
         # Frame Columns
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
+
+        # Load Icons
+        self.paste_icon = ctk.CTkImage(Image.open(os.path.join(current_path, 'paste-light.png')),
+                                       size=(20, 20))
+        self.download_icon = ctk.CTkImage(Image.open(os.path.join(current_path, 'download.png')),
+                                          size=(20, 20))
 
         # Labels
         self.download_label = ctk.CTkLabel(self, text='Insert Video Link:', font=('roboto', 15))
@@ -39,12 +90,18 @@ class DownloadFrame(ctk.CTkFrame):
         # Entry
         self.download_entry = ctk.CTkEntry(self, border_color='#40F8FF', textvariable=self.url,
                                            validatecommand=self.is_valid)
-        self.download_entry.grid(row=0, column=0, columnspan=2, padx=(140, 10), pady=10, sticky='ew')
+        self.download_entry.grid(row=0, column=0, columnspan=2, padx=(140, 50), pady=10, sticky='ew')
         self.download_entry.bind('<KeyRelease>', self.is_valid)
 
         # Button
-        self.download_button = ctk.CTkButton(self, text='Download', command=self.on_download_button_click)
+        self.download_button = ctk.CTkButton(self, text='Download', image=self.download_icon, compound='left',
+                                             command=self.on_download_button_click)
         self.download_button.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
+
+        self.paste_button = ctk.CTkButton(self, text='', image=self.paste_icon, width=0,
+                                          fg_color='transparent', hover_color=('gray70', 'gray30'),
+                                          command=self.paste_button_event)
+        self.paste_button.grid(row=0, column=1, padx=10, pady=10, sticky='e')
 
         # Radio Buttons
         self.download_video = ctk.CTkRadioButton(self, text='Video', variable=self.radio_var, value=1)
@@ -114,10 +171,12 @@ class DownloadFrame(ctk.CTkFrame):
         else:
             self.download_status.configure(text='No Link Entered', text_color='#FE0000')
 
+    # Download Button Event Function
     def on_download_button_click(self):
         thread = threading.Thread(target=self.download)
         thread.start()
 
+    # Progressbar Function
     def on_progress(self, stream, chunk, bytes_remaining):
         total_size = stream.filesize
         bytes_downloaded = total_size - bytes_remaining
@@ -129,42 +188,14 @@ class DownloadFrame(ctk.CTkFrame):
         # Update Progress Bar
         self.download_progressbar.set(float(percentage_of_completion / 100))
 
+    # Create a Paste Button Function
+    def paste_button_event(self):
+        # Get the text from the clipboard
+        pasted_text = self.clipboard_get()
 
-class App(ctk.CTk):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # Place App in Center of Screen
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        x = (screen_width - appWidth) // 2
-        y = (screen_height - appheight) // 2
-
-        # Create Window
-        self.geometry(f'{appWidth}x{appheight}+{x}+{y}')
-        self.resizable(False, False)
-        self.title('YouTube Downloader')
-        self.iconbitmap('Icon.ico')
-
-        # Configure App Column
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-
-        # Configure App Row
-        self.rowconfigure(1, weight=1)
-
-        # Add App Label
-        self.app_label = ctk.CTkLabel(self, text='YOUTUBE DOWNLOADER\n VIDEO | AUDIO',
-                                      font=('sans serif', 20, 'bold'))
-        self.app_label.grid(row=0, column=0, columnspan=2, padx=10, pady=(10, 0), sticky='ew')
-
-        # Create Frame
-        self.downloadframe = DownloadFrame(self)
-        self.downloadframe.grid(row=1, column=0, columnspan=2, padx=10, pady=(10, 0), sticky='nsew')
-
-        # Author
-        self.author = ctk.CTkLabel(self, text='C.Sarmiento 2023 Version 1.0', font=('helvetica', 10))
-        self.author.grid(row=2, column=0, padx=10, sticky='w')
-        self.mainloop()
+        # Insert the text into the link entry widget
+        self.download_entry.delete(0, 'end')
+        self.download_entry.insert(0, pasted_text)
 
 
 if __name__ == '__main__':
